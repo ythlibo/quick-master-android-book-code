0d50554948d3f83272b09d519eb962b2cd91e82b
pingqing.ren <pingqing.ren@kanche.com>
2016-09-29 17:32:38 +0800

    add chapter2.1 code

---
+++ chapter2/section2.1/HelloComment/src/cn/woblog/book/android/Main.java
@@ -0,0 +1,18 @@
+/**
+ * 这是包名(这是一个文档注释)
+ */
+package cn.woblog.book.android;
+
+
+//定义一个公开的Main类(这是一个单行注释)
+public class Main {
+
+    /*
+        定义一个main方法(这是多行注释)
+     */
+    public static void main(String[] args) {
+        //打印一个字符串到控制台(这也是一个单行注释)
+        System.out.println("Hello World!");
+
+    }
+}
